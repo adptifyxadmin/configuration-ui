6fe2a999b4cb69d7549fe4c17d39759171a1f472
adptifyxadmin <adptifyx@gmail.com>
2025-06-05 22:34:36 +0530

    URL update

--- src/shared/utils/apiClient.ts
+++ src/shared/utils/apiClient.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_CONFIGURATION_API_URL || "https://hms-configuration-service-dqdfb8bsc4gygze9.eastasia-01.azurewebsites.net/api/config";
+const API_URL = 'https://app.adptifyx.com/configuration-service/api/config'
 
 const apiClient = axios.create({
   baseURL: API_URL,
